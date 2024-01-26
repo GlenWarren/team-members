ed8485e6aea3f60a8bff33b32b6bef0756b414e0
Glen Warren <glenwarren@zoho.com>
2024-01-26 12:21:02 +0000

    routes and UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function show(string $id)
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update(string $id)
+    {
+
+    }
+
+    public function destroy(string $id)
+    {
+
+    }
+}
