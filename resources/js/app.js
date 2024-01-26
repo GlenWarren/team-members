e17adc839dd1ba769fe3d8de02295325869c4270
Glen Warren <glenwarren@zoho.com>
2024-01-26 18:29:09 +0000

    Installed Vue js and started creating a component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import {createApp} from 'vue'
+import TeamMembersList from './components/TeamMembersList.vue';
+
+createApp({
+    components: {
+        TeamMembersList,
+    }
+}).mount('#app');
