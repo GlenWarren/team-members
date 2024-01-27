857d4681e9b669db217055d660392c9ed16d9d20
Glen Warren <glenwarren@zoho.com>
2024-01-27 00:39:47 +0000

    Beautified

--- resources/js/components/CreateUserForm.vue
+++ resources/js/components/CreateUserForm.vue
@@ -1,24 +1,28 @@
 <template>
-    <h1>
-        Profile
-    </h1>
-    <p class="grey">
-        This information will be displayed publicly so be careful what you share.
-    </p>
-    <div>
-        <form @submit.prevent="saveFormData">
-            <label for="name">Name:</label>
-            <input type="text" id="name" v-model="formData.name" required>
-            <br><br>
-            <label for="email">Email:</label>
-            <input type="email" id="email" v-model="formData.email" required>
-            <br><br>
-            <label for="image_url">Image Url:</label>
-            <input type="text" id="image_url" v-model="formData.image_url" required>
-            <br><br>
-            <button type="button" @click="cancelForm">Cancel</button>
-            <button type="submit">Save</button>
-        </form>
+    <div class="component">
+        <h3>
+            Profile
+        </h3>
+        <p class="grey blurb">
+            This information will be displayed publicly so be careful what you share.
+        </p>
+        <div>
+            <form @submit.prevent="saveFormData">
+                <label for="name">Name</label>
+                <input type="text" id="name" v-model="formData.name" required>
+
+                <label for="email">Email</label>
+                <input type="email" id="email" v-model="formData.email" required>
+
+                <label for="image_url">Image Url</label>
+                <input type="text" id="image_url" v-model="formData.image_url" required>
+
+                <div class="buttons">
+                    <button type="button" class="cancel-button" @click="cancelForm">Cancel</button>
+                    <button type="submit" class="save-button">Save</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
